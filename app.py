55c0d5e1f22310c4ba0026eb51808adf57dda70f
Izhar-Ali-Chachar <izharizharali057@gmail.com>
2025-07-19 11:20:33 +0500

    fastapi code added

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from agent.agentic_workflow import GraphBuilder
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Annotated
+from pprint import pprint
 
-app = GraphBuilder()
+app = FastAPI()
 
-graph = app.build_graph()
+class UserQuery(BaseModel):
+    user_input: Annotated[str, Field(..., description='user gives input')]
 
-result = graph.invoke({
-    "messages": [{"role": "user", "content": "What’s the budget for a 7-day solo trip to Thailand?"}]
-})
+@app.get('/')
+def home():
+    return {'message': 'home of web'}
 
-print(result)
+@app.post('/generate')
+def generator(user_query: UserQuery):
+    graph_builder = GraphBuilder()
+    graph = graph_builder.build_graph()
+
+    input = user_query.user_input
+
+    result = graph.invoke({
+        "messages": [{"role": "user", "content": input}]
+    })
+
+    return {"response": result["messages"][-1].content}
